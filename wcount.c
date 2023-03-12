2a43737095691aef144c3e12d4a1910a87fb1186
johtoleague <jon.doanne@gmail.com>
2023-03-12 10:33:20 -0500

    s

--- wcount.c
+++ wcount.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define IN 1 /* inside a word*/
-#define OUT 0 /* outside a word*/
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
 
 int main()
 {
@@ -10,9 +10,35 @@ int main()
     state = OUT;
     nl = nw = nc = 0;
     while ((c = getchar()) != EOF)
+    {
+        ++nc;
+        if (c == '\n')
         {
-            
+            ++nl;
         }
 
-   
-} 
+        // If current character is a whitespace character
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            // If we were inside a word previously, we have reached the end of a word
+            if (state == IN)
+            {
+                state = OUT;
+                ++nw; // Increment word count
+                putchar('\n');
+            }
+        }
+        // If current character is not a whitespace character
+        else
+        {
+            // If we were outside a word previously, we have entered a new word
+            if (state == OUT)
+            {
+                state = IN;
+            }
+            putchar(c);
+        }
+    }
+
+    printf("Line Count = %d \nChar Count = %d \nWord Count = %d\n", nl, nc, nw);
+}
