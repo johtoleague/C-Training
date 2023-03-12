3715f03cbc73f54ff8797b46a648f19da8ce56ec
johtoleague <jon.doanne@gmail.com>
2023-03-11 23:50:49 -0600

    s

---
+++ wcount.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define IN 1 /* inside a word*/
+#define OUT 0 /* outside a word*/
+
+int main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+        {
+            
+        }
+
+   
+} 
