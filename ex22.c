be52f44a9665767c43a3d73706fa996f86979611
johtoleague <jon.doanne@gmail.com>
2023-03-16 16:21:28 -0500

    s

---
+++ ex22.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define MAXLINE 1024
+int main()
+{
+
+int i,c;
+int lim = MAXLINE;
+char s[MAXLINE];
+
+    for (i = 0; (c = getchar()) != EOF; ++i){
+        if (c != '\n'){
+             if ( i<lim-1){
+                s[i] = c;
+             }
+            else{
+                break;
+            }
+        }
+        else{
+            break;
+        }
+    }
+    s[i] = '\0';  
+    printf("%s\n", s)
+
+}
