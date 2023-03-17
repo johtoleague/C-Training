874b76e888e5118f7b88053a812f0b1d1762e678
johtoleague <jon.doanne@gmail.com>
2023-03-17 16:14:26 -0500

    dd

--- htoi.c
+++ htoi.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+#include <math.h>
+#define MAXLINE 1024
 
 int getlines(char line[], int maxline);
-htoi(s);
+void htoi(int to[], char from[], int len);
+
 int i, c;
 char s[1024];
 char d[1024];
 
 int main()
 {
-    printf("Enter Hexadecimal value: ");
-    while ((c=getchar()) != '\n '){
-        if (count)
-    }
-
-    }
+	int len;
+	int max;
+	char line[MAXLINE];
+	char out[MAXLINE];
+	
+	while((len = getlines(line, MAXLINE)) > 0)
+		{
+			htoi(out, line, len);
+			printf("%s", out);
+		}
+	return 0;
 }
 
-int htoi(char s)
-{
-    return 0;
-}
+
 
 int getlines(char s[], int lim)
 {
@@ -32,4 +37,29 @@ int getlines(char s[], int lim)
 		s[i] = '\0';
 		return i;
 	}
+}
+
+void htoi(int* dec, char from[], int len)
+{
+    int i;
+    int value = 0;
+
+    for (i = len - 1; i >= 0 && from[i] != '\0'; --i)
+    {
+        if (from[i] >= '0' && from[i] <= '9') {
+            value += (from[i] - '0') * pow(16, len - i - 1);
+        }
+        else if (from[i] >= 'a' && from[i] <= 'f') {
+            value += (from[i] - 'a' + 10) * pow(16, len - i - 1);
+        }
+        else if (from[i] >= 'A' && from[i] <= 'F') {
+            value += (from[i] - 'A' + 10) * pow(16, len - i - 1);
+        }
+        else {
+            value = 0;
+            break;
+        }
+    }
+
+    *dec = value;
 }
