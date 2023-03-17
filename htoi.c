6961380523849ffc1d70e373681e9756ef20667a
johtoleague <jon.doanne@gmail.com>
2023-03-17 11:46:34 -0500

    d

---
+++ htoi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int getlines(char line[], int maxline);
+htoi(s);
+int i, c;
+char s[1024];
+char d[1024];
+
+int main()
+{
+    printf("Enter Hexadecimal value: ");
+    while ((c=getchar()) != '\n '){
+        if (count)
+    }
+
+    }
+}
+
+int htoi(char s)
+{
+    return 0;
+}
+
+int getlines(char s[], int lim)
+{
+	int c,i;
+	
+	for(i=0; i<lim-1 &&(c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+	{
+		s[i] = '\0';
+		return i;
+	}
+}
