3f5dcce2888b09936ffcdf34ec6bed71936c5bb7
johtoleague <jon.doanne@gmail.com>
2023-03-19 12:44:06 -0500

    nnn

--- binslowsearch.c
+++ binslowsearch.c
@@ -26,8 +26,7 @@ int main() {
 
     return 0;
 }
-
-int binsearch( int x, int v[], int n)
+/*int binsearch( int x, int v[], int n)
 {
     int low, high, mid;
 
@@ -42,5 +41,22 @@ int binsearch( int x, int v[], int n)
         else 
             return mid;
     }
-    return -1; /*no match*/
+    return -1; /*no match*/ 
+int binsearch( int x, int v[], int n){
+    int low, high, mid;
+    low = 0;
+    high = n-1;
+    mid = (low + high) /2;
+     while ( low <= high ){
+        mid = (low+high) / 2;
+        if (x < v[mid])
+            high = mid -1;
+        else 
+            low = mid +1;
+        if (v[mid] ==x){
+            break;
+        }
+       
+    }
+    return (v[mid] ==x) ? mid : -1;
 }
