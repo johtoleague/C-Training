26bc051b3cc8fcf86e8390713836b1b30ae0ef3b
johtoleague <jon.doanne@gmail.com>
2023-03-21 21:35:38 -0500

    mm,mm

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,44 @@
+#include <stdio.h>
+#include <string.h>
 
-        case '\t':
+void expand(char s1[], char s2[]) 
+{
+   
+    int i, j;
+    char c;
+    i = j = 0;
+    while ((c = s1[i++]) != '\0'){
+        printf("%c", c);
+        if (s1[i] == '-' && s1[i + 1] >= c)
+        {
+            printf("%c", s1[i]);
+            printf(" this is > %c < inside if statment" , c);
+            printf("%i inside if", i);
+            i++;
+            while (c <= s1[i]) {
+                s2[j++] = c++;
+            }
+        }
+        else
+        {
+            s2[j++] = c;
+        }
+
+    }
+    s2[j] = '\0';
+}
+
+int main()
+{
+    char b[] = "a-h";
+    char r[100];
+
+    expand(b, r);
+
+   // printf("%s",r);
+
+
+
+
+
+}
