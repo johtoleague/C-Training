4ae676ca96221c969294388c351179554cb1eecc
johtoleague <61567949+johtoleague@users.noreply.github.com>
2023-03-24 22:02:08 +0000

    jjj

--- atof.c
+++ atof.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+#include <ctype.h>
 
-/*atof : convert string s to double */
-
-double atof (char s[])
-{
+/* atof: convert string s to double */
+double atof(char s[]) {
     double val, power;
     int i, sign;
 
@@ -16,11 +15,17 @@ double atof (char s[])
         val = 10.0 * val + (s[i] - '0');
     if (s[i] == '.')
         i++;
-    for ( power = 1.0; isdigit(s[i]);i++) 
-    {
+    for (power = 1.0; isdigit(s[i]); i++) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10.0;
-
     }
-    return sign * val /power;
+    return sign * val / power;
+}
+
+int main() {
+    char s[] = "123.456";
+    double result = atof(s);
+
+    printf("The converted double value is: %f\n", result);
+    return 0;
 }
