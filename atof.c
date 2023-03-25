2c0d1c09e54a0fe74b9de632735c46395254dabe
johtoleague <jon.doanne@gmail.com>
2023-03-24 20:59:02 -0500

    kksss

--- atof.c
+++ atof.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <ctype.h>
+#define MAXLINE 100
 
-/* atof: convert string s to double */
+int getline(char line[], int max);
+double atof(char s[]);
+
+int main()
+{
+    double sum;
+    char line[MAXLINE];
+
+    sum = 0;
+    while (getline(line, MAXLINE) > 0) {
+        printf("\t%g\n", sum += atof(line));
+    }
+    return 0;
+}
+
 double atof(char s[]) {
     double val, power;
-    int i, sign;
+    int i, sign, exp_sign, exponent;
 
     for (i = 0; isspace(s[i]); i++)
         ;
@@ -19,13 +34,34 @@ double atof(char s[]) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10.0;
     }
-    return sign * val / power;
+    val = sign * val / power;
+
+    if (s[i] == 'e' || s[i] == 'E') {
+        i++;
+
+        exp_sign = (s[i] =='-') ? -1.0 : 1.0;
+        if(s[i] == '+' || s[i] == '-')
+        i++;
+
+        for (exponent = 0; isdigit(s[i]); i++)
+            exponent = 10 * exponent + (s[i] - '0');
+
+        val *= pow(10, exp_sign * exponent);
+    }
+
 }
 
-int main() {
-    char s[] = "123.456";
-    double result = atof(s);
+int getline(char s[], int lim)
+{
+    int c, i;
 
-    printf("The converted double value is: %f\n", result);
-    return 0;
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = '\0';
+        return i;
+    } else {
+        s[i] = '\0';
+        return i;
+    }
 }
