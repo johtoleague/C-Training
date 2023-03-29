337e93061b9425298d1d661e6c51d05bd619fee3
johtoleague <jon.doanne@gmail.com>
2023-03-29 17:01:35 -0500

    werewr

---
+++ getint.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int getch(void);
+void ungetch(int);
+
+/*getint get next integer from input into *pn */
+
+int getint(int *pn)
+{
+    int c, sign;
+
+    while (issspace(c = getch())) //skip spaces
+        ;
+    if (!digit(c) && c != EOF && c != '+' && c != '-')
+    {
+        ungetch(c); //not a number go back 1
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c =='+' || c == '-')
+        c == getch(); //get next character
+    for (*pn )
+}
