c4628d198c2678226ad3df5b22dcdafdf9b1ef4a
Jon Doanne <61567949+johtoleague@users.noreply.github.com>
2023-04-05 15:04:48 +0000

    sdf

--- strend2.c
+++ strend2.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 
-int strendd(char * s, char * t)
+int strleng(char *s);
+
+int strend(char *s, char *t)
 {
-    slen = strleng(*s)
-    tlen = strleng(*t)
-    int diff = slen - tlen
-    if ((diff) < 0){
+    int slen = strleng(s);
+    int tlen = strleng(t);
+    int diff = slen - tlen;
+    if (diff < 0) {
         return 0;
     } 
-    while ((s* + diff) == *t){
-        if (*t == '\0')
-        {
+    while ((*s + diff) == *t) {
+        if (*t == '\0') {
             return 1;
         }
         s++;
         t++;
-        }
-        else
-            return 0;
     }
-
+    return 0;
 }
 
-
-/*strlen using a pointer and trurn the length of the string*/
+/* strlen using a pointer and returns the length of the string */
 int strleng(char *s)
 {
     char *p = s;
-
-    while (*p != '\0')
-        p++; // pushing the index of p forward froim *s, which will point to zero
-    return p - s; /* lets say p will be moved forward 4 times 
-    and it will subtract 0, which is now pointing at the
-     first character aka the address of the string you get
-      the length */
+    while (*p != '\0') {
+        p++; // push the index of p forward from *s, which will point to zero
+    }
+    return p - s; /* let's say p will be moved forward 4 times 
+                    and it will subtract 0, which is now pointing at the
+                    first character aka the address of the string you get
+                    the length */
 }
