05363e89f5621d0f78528de45363f864db697023
Jon Doanne <61567949+johtoleague@users.noreply.github.com>
2023-04-06 15:42:20 +0000

    vv

--- dateconv.c
+++ dateconv.c
@@ -4,7 +4,7 @@ in leap year*/
 static char daytab[2][13] = {
     {0, 31, 28,31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29,31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-}
+};
 
 /* day_of_yar: set day of year from month & day */
 
