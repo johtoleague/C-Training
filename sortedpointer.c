8138a3f00e0a0fc716e63a7d0f99817bbc0c21c1
johtoleague <jon.doanne@gmail.com>
2023-04-05 21:26:54 -0500

    d

--- sortedpointer.c
+++ sortedpointer.c
@@ -17,6 +17,55 @@ main()
 
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
     {
-
+        qsort(lineptr, 0, nlines-1);
+        writelines(lineptr, nlines);
+        return 0;
     }
+    else 
+    {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
+
+#define MAXLEN 1000 /*max input lengt*/
+int getline(char *, int);
+char *alloc(int);
+
+/*readlines*/
+int readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = getline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else {
+            line[len-1] = '\0'; /*delete new line*/
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+/*writelines; white ouput lines*/
+void writelines(char *lineptr[], int nlines)
+{
+    int i;
+    for (i = 0; i< nlines; i++)
+        printf("%s\n", lineptr[i]);
+}
+
+int getline(char s[], int lim)
+{
+	int c,i;
+	
+	for(i=0; i<lim-1 &&(c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+	{
+		s[i] = '\0';
+		return i;
+	}
 }
