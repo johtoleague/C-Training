56b933de904ec4e5bce15efdb4f2edbbd7a15b2f
johtoleague <jon.doanne@gmail.com>
2023-04-05 21:38:53 -0500

    d

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,71 @@
+#include <stdio.h>
+#include <string.h>
 
+#define MAXLINES 5000
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+/*sort input lines*/
+
+main()
+{
+    int nlines; /* number of input lines read*/
+
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
+    {
+        qsort(lineptr, 0, nlines-1);
+        writelines(lineptr, nlines);
+        return 0;
+    }
+    else 
+    {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+}
+
+#define MAXLEN 1000 /*max input lengt*/
+int getline(char *, int);
+char *alloc(int);
+
+/*readlines*/
+int readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while ((len = getline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else {
+            line[len-1] = '\0'; /*delete new line*/
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+/*writelines; white ouput lines*/
+void writelines(char *lineptr[], int nlines)
+{
+    int i;
+    for (i = 0; i< nlines; i++)
+        printf("%s\n", lineptr[i]);
+}
+
+int getline(char s[], int lim)
+{
+	int c,i;
+	
+	for(i=0; i<lim-1 &&(c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+	{
+		s[i] = '\0';
+		return i;
+	}
 }
