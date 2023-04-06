84645f7b4d54ea6594d65f83649830f4ff95b98c
Jon Doanne <61567949+johtoleague@users.noreply.github.com>
2023-04-06 15:41:58 +0000

    d

--- dateconv.c
+++ dateconv.c
@@ -14,4 +14,19 @@ int day_of_year(int year, int month, int day)
 
     leap = year%4 == 0 && year%100 != 0 || year&400 == 0;
     for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
+
+/*month_day: set month, day from day of year */
+void month_day(int year , int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100!= 0 || year&400 == 0;
+    for (i = 1;yearday > daytab[leap][i]; i++);
+        yearday -= daytab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
+
 }
