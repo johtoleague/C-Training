1e8e1ef126e6de81fdd70dd4bf09152c7119d8e0
johtoleague <jon.doanne@gmail.com>
2023-04-07 23:34:45 -0500

    why we dont need to deref the pointer for printf

--- echo.c
+++ echo.c
@@ -7,7 +7,11 @@ main(int argc, char *argv);
 
     for (i = 1; i < argc; i++) {
         printf("%s%s", argv[i], (i < argc-1) ? " " : "");
-    }
+    }/* print argv[i], which will be the string the pointer points to
+    and then print the space if the argc is less than the 
+    last count of the string, 
+    the reason why we don't need to dereference the pointer argv[i]
+    is because printf() expects an address pointer, not the value*/
     printf("\n");
     return 0;
 } 
