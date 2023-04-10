24387d67d240a5b8ffc51d6d112e916877350450
johtoleague <61567949+johtoleague@users.noreply.github.com>
2023-04-10 13:17:54 -0600

    v

--- sortlinespointers.c
+++ sortlinespointers.c
@@ -30,3 +30,24 @@ main(int argc, char *argv[])
     }
 }
 
+
+/* qsort: sort v[letf]...v[right] into increasing order*/
+void qsort(void *v[], int left, int right,
+            int (*comp)(void *, void *))
+{
+    int i, last;
+    void swap(void *v[], int, int);
+
+    if (left >= right) /* do nothing if array contains*/
+        return;
+    swap(v, left, (left + right)/2);
+    last = left;
+    for (i = left+1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0)
+            swap(v,++last, i);
+    swap(v, left, last);
+    qsort(v, left, last-1, comp);
+    qsort(v, last+1, right, comp);
+
+
+}
