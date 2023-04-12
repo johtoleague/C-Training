2f38226c8ca143712e7d1fc29dc831b572f49dad
johtoleague <jon.doanne@gmail.com>
2023-04-12 15:24:44 -0500

    ls

---
+++ KRCProgramming/structs/point.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Point {
+    double x;
+    double y;
+};
+
+void Point_print(const struct Point* self);
+
+int main()
+{
+    struct Point aPoint;
+    aPoint.x = 1.0;
+    aPoint.y = 3.0;
+    Point_print(&aPoint);
+
+}
+
+void Point_print(const struct Point *self)
+{
+    printf("%f, %f)\n", self->x, self->y);
+}
